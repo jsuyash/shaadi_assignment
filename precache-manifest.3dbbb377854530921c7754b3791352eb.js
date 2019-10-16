87fe5afba614d7f3fa6b74af0e770c9316b000ba
Suyash jadhav <suyash.jadhav@crispanalytics.com>
2019-10-16 14:59:45 +0530

    Updates

---
+++ precache-manifest.3dbbb377854530921c7754b3791352eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1faae0684ce9b4c33585aa6ae0a55b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02dd32331c68502eb0b1",
+    "url": "/static/css/main.e36caf04.chunk.css"
+  },
+  {
+    "revision": "2b4f29b407c5540a9c30",
+    "url": "/static/js/2.96ff4a19.chunk.js"
+  },
+  {
+    "revision": "02dd32331c68502eb0b1",
+    "url": "/static/js/main.0406df42.chunk.js"
+  },
+  {
+    "revision": "c106e39cc5be40c1b44d",
+    "url": "/static/js/runtime-main.7705e6e0.js"
+  }
+]);
