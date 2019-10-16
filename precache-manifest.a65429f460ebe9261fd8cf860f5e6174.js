5e13ff52ec984fd1b4beaabfa17b6c6443341e3e
Suyash jadhav <suyash.jadhav@crispanalytics.com>
2019-10-16 17:35:30 +0530

    Updates

---
+++ precache-manifest.a65429f460ebe9261fd8cf860f5e6174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc145939376651616e59b27dc0b6ad99",
+    "url": "/shaadi_assignment/index.html"
+  },
+  {
+    "revision": "62e8fa0b558b06a3d92d",
+    "url": "/shaadi_assignment/static/css/main.f0e68b61.chunk.css"
+  },
+  {
+    "revision": "2b4f29b407c5540a9c30",
+    "url": "/shaadi_assignment/static/js/2.96ff4a19.chunk.js"
+  },
+  {
+    "revision": "62e8fa0b558b06a3d92d",
+    "url": "/shaadi_assignment/static/js/main.80b4d755.chunk.js"
+  },
+  {
+    "revision": "985287ccc15c9390180a",
+    "url": "/shaadi_assignment/static/js/runtime-main.7864c538.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dbbb377854530921c7754b3791352eb.js"
+  "/shaadi_assignment/precache-manifest.a65429f460ebe9261fd8cf860f5e6174.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shaadi_assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
