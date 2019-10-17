c03a9abaab3da6c976de2c03e42a1be1a90d5236
Suyash jadhav <suyash.jadhav@crispanalytics.com>
2019-10-17 15:05:52 +0530

    Fix Build Issue By Adding Basepath To Router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.scss";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <PageRoutes />
       </div>
--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,13 +6,11 @@ import CarouselPage from "./pages/CarouselPage";
 
 const PageRoutes = () => {
   return (
-    <Suspense>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/carousel" component={CarouselPage} />
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/carousel" component={CarouselPage} />
+    </Switch>
   );
 };
 
